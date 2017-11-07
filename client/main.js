64e337df8ba36182530459f9ba72043e446fcd22
Macgyver Martins <macgyver.martins@gmail.com>
2017-11-07 13:49:36 -0300

    init client

--- client/main.js
+++ client/main.js
@@ -1,5 +1,33 @@
-import {greetUser, name} from './../imports/utils';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 
-console.log(greetUser());
-console.log(name);
-console.log('Log from /client/main.js');
+Meteor.startup(function() {
+  const players = [{
+    _id: '1',
+    name: 'Mac',
+    score: 99
+  },{
+    _id: '2',
+    name: 'Leticia',
+    score: 97
+  },{
+    _id: '3',
+    name: 'Marcia',
+    score: 98
+  }];
+
+  const renderPlayers = function(playersList) {
+    return playersList.map(function(player) {
+      return <p key={player._id}>{player.name}</p>;
+    });
+  };
+
+  let jxs = (
+    <div>
+      <p>This is from main.js</p>
+      {renderPlayers(players)}
+    </div>
+  );
+  ReactDOM.render(jxs, document.getElementById('app'));
+});
