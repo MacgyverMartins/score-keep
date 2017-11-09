e29b60abc84c1fb13788ec2941ed053259a7b4a2
Macgyver Martins <macgyver.martins@gmail.com>
2017-11-09 20:58:34 -0300

    Add basic behavior

--- client/main.js
+++ client/main.js
@@ -5,21 +5,43 @@ import { Tracker } from 'meteor/tracker';
 import { Players } from './../imports/api/players';
 
 
-Meteor.startup(function() {
+const renderPlayers = (playersList) => {
+  return playersList.map((player) => {
+    return (
+      <p key={player._id}>
+        {player.name} as {player.score} points.
+        <button onClick={() => {
+          Players.update(player._id, { $inc: {score: -1} });
+        }}>-1</button>
+        <button onClick={() => {
+          Players.update(player._id, { $inc: {score: 1} });
+        }}>+1</button>
+        <button onClick={() => Players.remove(player._id)}>X</button>
+      </p>
+    );
+  });
+};
 
-  Tracker.autorun(function() {
+const handleSubimit = function(e) {
+  let playerName = e.target.playerName.value;
+  e.preventDefault();
+  Players.insert({
+    name: playerName,
+    score: 0
+  });
+}
+
+Meteor.startup(() => {
+  Tracker.autorun(() => {
     const players = Players.find().fetch();
-
-    const renderPlayers = function(playersList) {
-      return playersList.map(function(player) {
-        return <p key={player._id}>{player.name} as {player.score} points</p>;
-      });
-    };
-
     let jxs = (
       <div>
-      <p>This is from main.js</p>
-      {renderPlayers(players)}
+        <p>This is from main.js</p>
+        {renderPlayers(players)}
+        <form onSubmit={handleSubimit}>
+          <input type="text" name="playerName" placehoulder="Player name" />
+          <button>Add Player</button>
+        </form>
       </div>
     );
     ReactDOM.render(jxs, document.getElementById('app'));
--- server/main.js
+++ server/main.js
@@ -1,11 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import { Players } from './../imports/api/players';
 
-Meteor.startup(function() {
-  Players.insert({
-    name: 'Mac',
-    score: 3
-  });
-
-  console.log(Players.find().fetch());
+Meteor.startup(() => {
 });
