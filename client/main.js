7e96918b3d67bf196752ed70fe7b7ebecf308f1b
Macgyver Martins <macgyver.martins@gmail.com>
2017-11-10 14:07:47 -0300

    Improve add buttom

--- client/main.js
+++ client/main.js
@@ -13,11 +13,11 @@ const renderPlayers = (playersList) => {
         <button onClick={() => {
           Players.update(player._id, { $inc: {score: -1} });
         }}>-1</button>
-        <button onClick={() => {
-          Players.update(player._id, { $inc: {score: 1} });
-        }}>+1</button>
-        <button onClick={() => Players.remove(player._id)}>X</button>
-      </p>
+      <button onClick={() => {
+        Players.update(player._id, { $inc: {score: 1} });
+      }}>+1</button>
+    <button onClick={() => Players.remove(player._id)}>X</button>
+  </p>
     );
   });
 };
@@ -31,11 +31,22 @@ const handleSubimit = function(e) {
   });
 }
 
+class TitleBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>My app</h1>
+      </div>
+    )
+  }
+}
+
 Meteor.startup(() => {
   Tracker.autorun(() => {
     const players = Players.find().fetch();
     let jxs = (
       <div>
+        <TitleBar/>
         <p>This is from main.js</p>
         {renderPlayers(players)}
         <form onSubmit={handleSubimit}>
